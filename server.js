4c054d61676b6cb5a425159f7500093800ef69c9
Brian <Brian@Brians-MacBook-Air.local>
2019-04-24 15:06:22 -0500

    express and mongoose up and running

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const session = require('express-session')
+
+
+require("./db/db")
+
+
+// middleWare
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static('public'))
+app.use(methodOverride('_method'));
+app.use(session({
+  secret: 'asdlfkjalsdkfjalsdkjfapw983u2357028euqoih23597357qjkweh',
+  saveUninitialized: false,
+  resave: false
+}));
+
+//controller
+
+
+
+
+app.listen(3000, () => {
+  console.log("server listening on 3000");
+})
