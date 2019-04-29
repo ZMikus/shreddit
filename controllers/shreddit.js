e74ee3cf0c08b3f4a82341c96a0197eab24a51fd
Brian <Brian@Brians-MacBook-Air.local>
2019-04-29 14:32:21 -0500

    cleaned up code

--- controllers/shreddit.js
+++ controllers/shreddit.js
@@ -7,24 +7,10 @@ const Activities = require('../models/activities')
 
 
 router.post('/', async (req, res, next) => {
-	console.log(req.session)
-	console.log(req.body)
+
 	try{
-		// if(req.body.weights === 'on'){
-		// 	const createdPlan = Plan.create(req.body, (err, createdPlan)=>{
+		const createdPlan = Plan.create(req.body, (err, createdPlan)=>{
 		// 	createdPlan.name = 'weights'
-
-
-			// workout.week = week 1
-			// workout.day = day 1
-			// activities.type = 'weights'
-			// activities.duration = 'null'
-			//activities.quantities = 10
-			//activies.name = "bench-press"
-
-			// end of workout
-
-
 
 			// create the plan		
 
@@ -32,192 +18,80 @@ router.post('/', async (req, res, next) => {
 					/// this could be up to 20 lines of code
 
 				// store those days in an array
-const workoutDays = [
-                    'Monday',
-                    'Wednesday',
-                    'Friday'   
-                ] // convert this to array of date objects
+				// convert this to array of date objects
+			const days = [
+				"Monday",
+				"Tusday",
+				"Wedensday",
+				"Thursday",
+				"Friday",
+				"Saturday",
+				"Sundary"
+			]
+			// figure out all the types of workouts the user wants req.body
+			const prefs = ['weights', 'plyo', 'cardio'] 
 
-const offDays = [
-					'Tuesday',
-					'Thursday',
-					'Saturday',
-					'Sunday'
-				]
-
-
-
-
-				// figure out all the types of workouts the user wants req.body
+			// put those types in an array
 
 
-
-				// put those types in an array
-
-
-	
-
-		// if(req.body.weights === 'on'){
+		console.log("------------------------");
+		console.log(activities)
 			
-
-		// }
-		// if(req.body.cardio === 'on'){
-
-
-		// }
-		// if(req.body.plyos === 'on'){
-
-		// }
-
-
-	console.log("------------------------");
-
-	const prefs = ['weights', 'plyo', 'cardio']
-	
-	const howManyActivitiesYouWant = 5
-	console.log(req.body.value);
-
-	// if(dayActivities.length % 3 <= 0){ 
-	for(let i = 0; i < howManyActivitiesYouWant; i++) { // dealing with one activity type at a time
-
+		const howManyActivitiesYouWant = 5;
+		const dayActivities = [];
+		for(let i = 0; i < howManyActivitiesYouWant; i++) { 
+		// use modulo, i, and prefs.length (HINT HINT) to programmatically cycle thru prefs
 		// this is the type
-		console.log(prefs[i % prefs.length])
 
 		// print the tyupe of activity we should be generating
+		console.log(prefs[i % prefs.length])
+	 // once it correctly cycles thru -- and no sooner -- get a random activity for the type
+		const randomActivityNumber = Math.floor(Math.random() * (activities.length));
+	 
+
+
+
+		
 
 	}
+			// loop over array of dates. in the loop:
+
+			// create a workout (all type of activities selected)
+
+			// set the workout day to be this date in the array
 
 
-	// for(let j = 0; j < activities.length; j++){
-	// 		console.log(dayActivities)
+					
 
-	// 	}
-		
-	// }
-			// use modulo, i, and prefs.length (HINT HINT) to programmatically cycle thru prefs
-
-	
-			// once it correctly cycles thru -- and no sooner -- get a random activity for the type
-			
-
-		// } // end try
-		// catch (err) {
-
-		// }
-
-
-
-
-				// loop over array of dates. in the loop:
-					// create a workout (all type of activities selected)
-					// set the workout day to be this date in the array
-
-
-					// loop through array of types the user selected  --- use while loop with modulus (modulate by the number of activities they selected)
-
-						// for each type:
+				// for each type:
 
 						// if there are not yet 3 activities
 							
 							// random activity:
 
-								// get the activities of this type from the database
+					// get the activities of this type from the database
 								// Math.random to get one of those 
 
 
-
-// const days = [
-//                     'Monday',
-//                     'Tuesday',
-//                     'Wednesday',
-//                     'Thursday',
-//                     'Friday',
-//                     'Saturday',
-//                     'Sunday',
-//                 ]
-
-
-
-
-
-			
-					
-				
-
-				
-				
-	// for(let i = 0 ; i < activites.length; i++){
-	// 			if(activities[i].type === 'weights'){
-	// 				userFocus.push(activities[i])
-	// 			}
-	// 		}
-	// 		for(let i = 0; i < activities.length; i++){
-	// 			if(activities[i].type === 'cardio'){
-	// 				userFocus.push(activities[i])
-	// 			}
-	// 		}
-	// 		for(let i = 0; i < activities.length; i++){
-	// 			if(activities[i].type === 'plyos'){
-	// 				userFocus.push(activities[i])
-	// 			}
-	// 		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			//workout.
-
-			
-		// 	res.render('index.ejs', {
+		//workout.// 	res.render('index.ejs', {
 		// 		plan: createdPlan
 		// 	})
 
 		// 	})
 		// }
-		// console.log('cool');
-		res.send('check terminal')
-	}catch(err){
+		
+		})	
+		 res.send('check terminal')
+		}catch(err){
 		next(err)
 	}
 })
-// 	try{
-//     /// create the shreddit plan
-//     // info you need to create plan is in req.body
-//     // create workouts
-//     if(req.body.weights === 'on'){
-//         const createdPlan = Plan.create(req.body, (err, createdPlan)=>{
-// 				createdPlan.name = 'weights'
-  
-//         res.render('index.ejs', {
-//             plan: createdPlan
-//         })
-//     }) catch(err){
-//         next(err)
-//     }
-//     }
-    
-// 	}
-// })
 
 
 
 
 
-
-
- router.get('/select-plan', (req, res) => {
+router.get('/select-plan', (req, res) => {
 	res.render('selectPlan.ejs')
 })
 
